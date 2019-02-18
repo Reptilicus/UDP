f48f8f2306d15496d27e5d6204158970c1ca4be4
Reptilicus <Reptilicus@users.noreply.github.com>
2019-02-18 13:29:03 -0600

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,111 @@
+import java.awt.image.BufferedImage;
+
+import java.io.ByteArrayOutputStream;
+
+import java.io.File;
+
+import java.io.IOException;
+
+import java.net.DatagramPacket;
+
+import java.net.DatagramSocket;
+
+import java.net.InetAddress;
+
+import javax.imageio.ImageIO;
+
+import javax.xml.crypto.Data;
+
+public class Driver {
+
+    public static boolean correctFileType(String extension){
+        boolean flag = false;
+        switch (extension) {
+        }
+        return flag;
+    }
+
+    public static String getFileExtension(String fullName) {
+        String fileName = new File(fullName).getName();
+        int dotIndex = fileName.lastIndexOf('.');
+        return (dotIndex == -1) ? "" : fileName.substring(dotIndex + 1);
+    }
+    
+    public static void sendData(byte[] buffer, DatagramSocket ds, InetAddress ip ) {
+
+        int startOffsetValue, packetCounter = 1;
+        int counter = 0;
+        byte[] buff = new byte[(buffer.length / 12) + 1];
+        byte[] padded = new byte[buffer.length % (buffer.length / 12)];
+        
+        try {
+            for (int i = 0; i < buffer.length; i++) {
+                startOffsetValue = i;
+                if (packetCounter == 13) {
+                    for (int j = 0; j < padded.length - 1; j++) {
+                        padded[counter] = buffer[i];
+                        i++;
+                        counter++;
+                    }
+
+                    DatagramPacket buffPacket = new DatagramPacket(padded, padded.length, ip, 1234);
+                    ds.send(buffPacket);
+                    System.out.println(
+                            " Packet Number: " + packetCounter + "\t SO: " + startOffsetValue + "    \tEO: " + (i - 1));
+                } else {
+                    buff[counter] = buffer[i];
+                    counter++;
+                    startOffsetValue = i - (buffer.length / 12);
+                }
+                if (i % (buffer.length / 12) == 0 && i != 0) {
+
+                    // Creating the datagramPacket for sending the data.
+                    DatagramPacket bufferPacket = new DatagramPacket(buffer, buffer.length, ip, 1234);
+                    ds.send(bufferPacket);
+                    buff = new byte[(buffer.length / 12) + 1];
+                    counter = 0;
+                    System.out.println(
+                            " Packet Number: " + packetCounter + "\t SO: " + startOffsetValue + "     \tEO: " + (i - 1));
+                    packetCounter++;
+                }
+            }
+            ds.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        // Creating the socket object for carrying the data.
+        DatagramSocket ds = new DatagramSocket();
+        byte[] receiveData = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        String filePath = args[0];
+
+        // The image being passed in.
+        BufferedImage img = ImageIO.read(new File(filePath));
+        String fileExtension = Driver.getFileExtension(args[0]);
+        if (!correctFileType(fileExtension)) {
+            System.out.println("Incorrect file type. Please double check path and try again.");
+            System.exit(0);
+        }
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(img, fileExtension, outputStream);
+        outputStream.flush();
+
+        // the byte array that holds the image data.
+        byte[] buffer = outputStream.toByteArray();
+
+        // Uncomment next line to pull in ip address via cmd
+        // InetAddress ip = args[1];
+        
+        InetAddress ip = InetAddress.getLocalHost();
+
+        // Sending the data
+        Driver.sendData(buffer, ds, ip);
+        
+        // Receive data
+        ds.receive(receivePacket);
+    }
+}
---
+++ Server.java
@@ -0,0 +1,26 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+class Server {
+
+    public static void main(String args[]) throws Exception {
+    	boolean listening = true;
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+        while(listening) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String( receivePacket.getData());
+            System.out.println("RECEIVED: " + sentence);
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String capitalizedSentence = sentence.toUpperCase();
+            sendData = capitalizedSentence.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+        }
+        serverSocket.close();
+    }
+}
